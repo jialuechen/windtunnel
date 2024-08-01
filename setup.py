27221899fc833791db140a0928e694d22ea23737
Jialue Chen <jialuechen@outlook.com>
2024-08-01 01:47:40 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='deepfolio',
-    version='1.2.0',
+    version='1.2.2',
     packages=setuptools.find_packages(),
     install_requires=[],
     url='https://github.com/jialuechen/deepfolio',
