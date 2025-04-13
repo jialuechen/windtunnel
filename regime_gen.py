303c6c158b77a1088e56e17bf6c3352f565e7a7f
Jialue Chen <jialuechen@outlook.com>
2025-04-12 23:54:37 -0400

    added support for vae training

--- regime_gen.py
+++
@@ -1,9 +0,0 @@
-class RegimeGenerator:
-    def __init__(self, scenario_config):
-        self.config = scenario_config
-
-    def generate(self):
-        return {
-            "regime_vector": [0.1, 0.2, 0.3],
-            "regime_type": self.config["regime_type"]
-        }
