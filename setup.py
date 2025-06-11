98a2ddb41a30ea4d866c59d35b4e63ab6bb395e5
Jialue Chen <jialuechen@outlook.com>
2025-06-10 20:35:55 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='windtunnel',
     version='0.1.0',
     description='Retrieval-augmented conditional diffusion market simulator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Jialue Chen',
     author_email='jialuechen@outlook.com',
     url='https://github.com/jialuechen/windtunnel',
@@ -15,15 +17,20 @@ setup(
         'pandas',
         'scipy',
         'fastdtw',
-        'scikit-learn'
+        'scikit-learn',
+        'plotly',  
+        'openai', 
     ],
     extras_require={
-        'dev': ['pytest', 'black', 'flake8']
+        'dev': ['pytest', 'black', 'flake8', 'build', 'twine']
     },
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Office/Business :: Financial :: Investment',
     ],
     python_requires='>=3.8',
+    include_package_data=True,
 )
